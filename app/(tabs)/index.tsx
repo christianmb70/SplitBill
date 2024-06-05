60413b9e10edbfc492d470b0cf79c9a797497e4e
christianmb70 <christianmb70@gmail.com>
2024-06-05 15:41:42 -0700

    Updated index
    Updated index with style sheets and made it that the tip options only are allowed to be clicked one out of them.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, Button,TextInput, Keyboard} from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, Platform, Image, Text, View, Button,TextInput, Keyboard, TouchableOpacity} from 'react-native';
 
 export default function HomeScreen() {
   const [text, onChangeText] = React.useState('');
   const [Total, onChangeTotal] = React.useState('');
   const [People, onChangePeople] = React.useState('');
+  const [activeButton, setActiveButton] = React.useState(null);
 
   const handleDonePress = () => {
     Keyboard.dismiss();
   }; 
+
+  // const handleButtonPress = (percentage : any) => {
+  //   onChangeTotal((Number(Total) + Number(Total) * percentage).toFixed(2));
+  //   setActiveButton(percentage);
+  // };
+
+  const changeActiveButton = (percentage: any) => {
+    setActiveButton(percentage);
+  };
 
   return (
     <View style={styles.container}> 
@@ -33,7 +38,7 @@ export default function HomeScreen() {
         style={styles.input}
         onChangeText={onChangeTotal}
         value={Total}
-        placeholder="10.00"
+        placeholder="0"
         keyboardType="numeric"
         returnKeyType="done"
         onSubmitEditing={handleDonePress}
@@ -43,7 +48,7 @@ export default function HomeScreen() {
         style={styles.input}
         onChangeText={onChangePeople}
         value={People}
-        placeholder="10.00"
+        placeholder="0"
         keyboardType="numeric"
         returnKeyType="done"
         onSubmitEditing={handleDonePress}
@@ -51,31 +56,51 @@ export default function HomeScreen() {
       <Text style={styles.text}>Did you leave a Tip?</Text>
       
       <View style={styles.row}>
-        <View style={styles.button1}>
-          <Button
-            title="5%"
-            color="Black"
-          />
+        <View>
+          <TouchableOpacity 
+            style={[styles.touch, activeButton === 0.05 && styles.active]}
+            onPress={() => changeActiveButton(0.05)}
+          >
+            <View style={styles.textContainer}>
+              <Text>5%</Text>
+              <Text>{(Number(Total) * 0.05).toFixed(2)}</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-        <View style={styles.button1}>
-          <Button
-            title="10%"
-            color="Black"
-          />
+        <View>
+          <TouchableOpacity 
+            style={[styles.touch, activeButton === 0.10 && styles.active]}
+            onPress={() => changeActiveButton(0.10)}
+          >
+            <View style={styles.textContainer}>
+              <Text>10%</Text>
+              <Text>{(Number(Total) * 0.10).toFixed(2)}</Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
       <View style={styles.row}>
-        <View style={styles.button1}>
-          <Button
-            title="15%"
-            color="Black"
-          />
+        <View>
+          <TouchableOpacity 
+            style={[styles.touch, activeButton === 0.15 && styles.active]}
+            onPress={() => changeActiveButton(0.15)}
+          >
+            <View style={styles.textContainer}>
+              <Text>15%</Text>
+              <Text>{(Number(Total) * 0.15).toFixed(2)}</Text>
+            </View>
+          </TouchableOpacity>
         </View>
-        <View style={styles.button1}>
-          <Button
-            title="20%"
-            color="Black"
-          />
+        <View>
+          <TouchableOpacity 
+            style={[styles.touch, activeButton === 0.20 && styles.active]}
+            onPress={() => changeActiveButton(0.20)}
+          >
+            <View style={styles.textContainer}>
+              <Text>20%</Text>
+              <Text>{(Number(Total) * 0.20).toFixed(2)}</Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
 
@@ -86,7 +111,7 @@ export default function HomeScreen() {
         />
       </View>
       <View style={styles.button2}>
-        <Button 
+        <Button
           title="Submit"
           color="black"/>
       </View>
@@ -126,11 +151,28 @@ const styles = StyleSheet.create({
       fontStyle: 'normal',
       fontWeight: '700',
     },
+    touch: {
+      width: 70,
+      paddingLeft: 10,
+      paddingRight: 10,
+      paddingBottom: 5,
+      paddingTop: 5,
+      borderRadius: 10,
+      backgroundColor: 'rgba(190, 178, 160, 1)',
+    },
+    textContainer: {
+      alignItems: 'center', 
+      justifyContent: 'center', 
+    },
+    active: {
+      backgroundColor: 'rgba(183, 203, 191, 1)',
+    },
     button1: {
       paddingLeft: 10,
       paddingRight: 10,
       borderRadius: 10,
       backgroundColor: 'rgba(190, 178, 160, 1)',
+      alignContent: "center"
     },
     button2: {
       marginTop: 80,
